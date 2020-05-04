652517304a3d210de34742d21bc0936bdf799c69
Eeshwar Das <50190227+eeshwardas@users.noreply.github.com>
2020-05-04 16:48:56 -0700

    Add files via upload

---
+++ MakeIndexHtml.java
@@ -0,0 +1,7 @@
+package com.eeshvardas.claypotveda.Homepage;
+
+final class MakeIndexHtml {
+    private final java.lang.String html = "" +
+            "<p><dfn>Eeshvar Das</dfn> of <em>eeshvardas.com</em> is Las Vegas, NV resident " +
+            "<em>Erik Douglas Ward</em></p>";
+}
